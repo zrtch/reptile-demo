1ba2b54b8b9c8bcdf27a3719d9929ce144591c66
zrtch <1160639597@qq.com>
2022-07-11 22:01:50 +0800

    first commit

---
+++ src/crowller.ts
@@ -0,0 +1,20 @@
+//首先要创建一个爬虫的类
+// ts -> .d.ts 翻译文件 -> js
+import superagent from 'superagent' // 这里是js代码  得安装cnpm i --save-dev @types/superagent -D 
+
+class Crowller {
+    private secret = 'secretKey';
+    private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
+    private rawHtml = '';
+
+   async getRawHtml(){
+        const result = await superagent.get(this.url)
+        this.rawHtml = result.text
+    }
+
+    constructor(){
+        this.getRawHtml();
+    }
+}
+
+const crowller = new Crowller();
