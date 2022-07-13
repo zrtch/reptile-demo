82fc5fff86089d11fe91306ced27e58fdb2cd022
zrtch <1160639597@qq.com>
2022-07-14 00:24:32 +0800

    Update

--- src/crowller.ts
+++ src/crowller.ts
@@ -13,7 +13,7 @@ class Crowller {
     private secret = 'secretKey';
     private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
 
-    //数据生成
+    //数据生成方法
     getCourseInfo(html: string){
         const $ = cheerio.load(html)
         const courseItems = $('.course-item')
