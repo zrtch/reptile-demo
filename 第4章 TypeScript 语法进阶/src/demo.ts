16ac74f890bdc8a31f3b254ef769b11f59bf07ab
zrtch <1160639597@qq.com>
2022-08-22 23:13:29 +0800

    Update

---
+++ 第4章 TypeScript 语法进阶/src/demo.ts
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @Date: 2022-08-22 22:27:16
+ */
+
+
+// const teacher: string = null
+
+const teacher: string = null;
+
+function fn(name: string, age: number){
+}
+
+export const name = 'dell'
---
+++ 第4章 TypeScript 语法进阶/src/file.js
@@ -0,0 +1,6 @@
+/*
+ * @Descripttion: 
+ * @Date: 2022-08-22 22:53:18
+ */
+const a = ''
+console.log(a.b.c);
