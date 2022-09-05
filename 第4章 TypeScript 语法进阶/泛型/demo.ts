67346308a6a2f5f5ae9e00fa2c6b247e3ef1e122
zrtch <1160639597@qq.com>
2022-09-05 22:41:39 +0800

    feat:泛型

---
+++ 第4章 TypeScript 语法进阶/泛型/demo.ts
@@ -0,0 +1,20 @@
+// 泛型 generic 泛指的类型；泛型是你用这个方法的时候才会具体知道T是哪个类型
+function join<T>(first: T, second: T){
+    return `${first}${second}`
+}
+// 调用join这个方法的时候，指定ABC这个泛型具体类型的是string
+join<string>('1','1')
+join<number>(1,1)
+join<string>('1', 1) //number就不匹配了
+
+// T[] 或者 Array<T>
+function map<T>(params:Array<T>){
+    return params
+}
+map<string>(['123'])
+
+//多个泛型
+function join1<T,P>(first: T,second : P){
+    return `${first}${second}`
+}
+join1<number,string>(1,'1')
